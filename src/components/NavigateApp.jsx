75a4cc84c193b3e697d93467d20adec36bd050bf
unknown <mateox463@gmail.com>
2025-12-26 00:41:34 -0300

    Se agrega nuevo home con nuevos estilos en navbar

--- src/components/NavigateApp.jsx
+++ src/components/NavigateApp.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { NavLink } from "react-router-dom";
-import '../styles/header.css';
+import "../styles/header.css";
 import { FaAngleDown } from "react-icons/fa";
 import { HiMenuAlt3, HiX } from "react-icons/hi";
 
@@ -9,130 +9,196 @@ export function NavigateApp({ auth, logIn, logOut }) {
   const [dropdownOpen, setDropdownOpen] = useState(false);
 
   return (
-    <nav className="bg-onix w-full z-20 top-0 border-b border-default shadow-lg">
-      <div className="max-w-7xl flex items-center justify-between mx-auto px-4 py-3 md:py-4">
+    <>
+      <nav className="fixed w-full z-20 top-0 ">
+        <div className="max-w-7xl flex items-center justify-between mx-auto px-4 py-3 md:py-4">
+          <NavLink to="/" className="flex items-center space-x-3">
+            <span className="bg-gradient-to-r from-red-500 to-red-600 p-2 rounded self-center text-xl font-bold whitespace-nowrap text-white">
+              Entrename IÁ!
+            </span>
+          </NavLink>
 
-        {/* Logo */}
-        <NavLink
-          to="/"
-          className="flex items-center space-x-3"
-        >
-          <span className="bg-linear-to-r from-red-500 to-red-600 p-1 self-center text-xl font-bold whitespace-nowrap">
-            Entrename IÁ!
-          </span>
-        </NavLink>
+          <div className="hidden md:flex bg-dark-onix/80 backdrop-blur-sm  py-2 px-3 rounded-full border-2 border-onix md:items-center md:space-x-6 lg:space-x-8">
+            <ul className="flex flex-row items-center space-x-6 lg:space-x-8 font-medium">
+              <li>
+                <NavLink
+                  to="/"
+                  className="block text-white py-2 px-3 rounded hover:text-gray-300 transition-colors"
+                >
+                  Inicio
+                </NavLink>
+              </li>
 
-        {/* Derecha */}
-        <div className="flex md:order-2 items-center space-x-2 md:space-x-3">
-          {/* Boton login desktop */}
-          <button
-            onClick={() => (auth ? logOut() : logIn())}
-            className="hidden md:flex bg-white items-center text-gray-700 font-bold justify-center gap-x-2 text-sm lg:text-base rounded-lg hover:bg-gray-100 duration-300 transition-colors border px-6 lg:px-8 py-2.5 cursor-pointer"
-          >
-            {auth ? "Log Out" : "Log In"}
-          </button>
+              {/* Dropdown */}
+              <li className="relative">
+                <button
+                  onClick={() => setDropdownOpen(!dropdownOpen)}
+                  className="flex text-white items-center py-2 px-3 rounded hover:text-gray-300 cursor-pointer transition-colors"
+                >
+                  Temáticas{" "}
+                  <FaAngleDown
+                    className={`ml-1 transition-transform ${
+                      dropdownOpen ? "rotate-180" : ""
+                    }`}
+                  />
+                </button>
 
-          {/* Boton hamburguesa mobile */}
+                {dropdownOpen && (
+                  <div className="absolute z-10 mt-2 w-56 bg-white text-slate-900 rounded-lg shadow-lg">
+                    <NavLink
+                      to="/gymexercise"
+                      className="block px-4 py-2 hover:bg-gray-100 rounded-lg transition-colors"
+                      onClick={() => setDropdownOpen(false)}
+                    >
+                      Entrenamiento Personal
+                    </NavLink>
+                  </div>
+                )}
+              </li>
+
+              <li>
+                <NavLink
+                  to="/nosotros"
+                  className="block text-white py-2 px-3 rounded hover:text-gray-300 transition-colors"
+                >
+                  Nosotros
+                </NavLink>
+              </li>
+
+              <li>
+                <NavLink
+                  to="/contacto"
+                  className="block text-white py-2 px-3 rounded hover:text-gray-300 transition-colors"
+                >
+                  Contacto
+                </NavLink>
+              </li>
+
+              {auth && (
+                <li>
+                  <NavLink
+                    to="/admin"
+                    className="block text-white py-2 px-3 rounded hover:text-gray-300 transition-colors"
+                  >
+                    Admin
+                  </NavLink>
+                </li>
+              )}
+
+              <li>
+                <button
+                  onClick={() => (auth ? logOut() : logIn())}
+                  className="bg-white text-gray-700 font-bold text-sm lg:text-base rounded-lg hover:bg-gray-100 duration-300 transition-colors border px-6 lg:px-8 py-2.5 cursor-pointer"
+                >
+                  {auth ? "Log Out" : "Log In"}
+                </button>
+              </li>
+            </ul>
+          </div>
+
           <button
             type="button"
             onClick={() => setMenuOpen(!menuOpen)}
             className="inline-flex items-center p-2 w-10 h-10 justify-center text-white text-2xl rounded-lg md:hidden hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-600 transition-all"
           >
-            <span className="sr-only">{menuOpen ? "Cerrar menú" : "Abrir menú"}</span>
+            <span className="sr-only">
+              {menuOpen ? "Cerrar menú" : "Abrir menú"}
+            </span>
             {menuOpen ? <HiX /> : <HiMenuAlt3 />}
           </button>
         </div>
 
-        {/* Menu */}
-        <div
-          className={`${
-            menuOpen ? "flex" : "hidden"
-          } absolute md:relative top-full left-0 w-full md:w-auto md:flex md:order-1 bg-onix md:bg-transparent border-t md:border-t-0 border-gray-700`}
-        >
-          <ul className="flex flex-col md:flex-row md:space-x-6 lg:space-x-8 w-full md:w-auto p-4 md:p-0 font-medium">
-
-            <li>
-              <NavLink
-                to="/"
-                className="block text-white py-2 px-3 rounded hover:bg-gray-700 md:hover:bg-transparent md:hover:text-gray-300 transition-colors"
-                onClick={() => setMenuOpen(false)}
-              >
-                Inicio
-              </NavLink>
-            </li>
-
-            {/* Dropdown */}
-            <li className="relative">
-              <button
-                onClick={() => setDropdownOpen(!dropdownOpen)}
-                className="flex text-white items-center w-full py-2 px-3 rounded hover:bg-gray-700 md:hover:bg-transparent md:hover:text-gray-300 cursor-pointer transition-colors"
-              >
-                Temáticas <FaAngleDown className={`ml-1 transition-transform ${dropdownOpen ? 'rotate-180' : ''}`} />
-              </button>
-
-              {dropdownOpen && (
-                <div className="md:absolute relative z-10 md:mt-2 w-full md:w-56  bg-white text-slate-900 shadow-lg">
-                  <NavLink
-                    to="/gymexercise"
-                    className="block px-4 py-2 hover:bg-gray-100 rounded-lg transition-colors"
-                    onClick={() => {
-                      setDropdownOpen(false);
-                      setMenuOpen(false);
-                    }}
-                  >
-                    Entrenamiento Personal
-                  </NavLink>
-                </div>
-              )}
-            </li>
-
-            <li>
-              <NavLink
-                to="/nosotros"
-                className="block text-white py-2 px-3 rounded hover:bg-gray-700 md:hover:bg-transparent md:hover:text-gray-300 transition-colors"
-                onClick={() => setMenuOpen(false)}
-              >
-                Nosotros
-              </NavLink>
-            </li>
-
-            <li>
-              <NavLink
-                to="/contacto"
-                className="block text-white py-2 px-3 rounded hover:bg-gray-700 md:hover:bg-transparent md:hover:text-gray-300 transition-colors"
-                onClick={() => setMenuOpen(false)}
-              >
-                Contacto
-              </NavLink>
-            </li>
-
-            {auth && (
+        {menuOpen && (
+          <div className="md:hidden bg-onix/90 backdrop-blur-sm py-2 px-3  border-gray-700">
+            <ul className="flex flex-col w-full p-4 font-medium space-y-2">
               <li>
                 <NavLink
-                  to="/admin"
-                  className="block text-white py-2 px-3 rounded hover:bg-gray-700 md:hover:bg-transparent md:hover:text-gray-300 transition-colors"
+                  to="/"
+                  className="block text-white py-2 px-3 rounded hover:bg-gray-700 transition-colors"
                   onClick={() => setMenuOpen(false)}
                 >
-                  Admin
+                  Inicio
                 </NavLink>
               </li>
-            )}
 
-            {/* Boton login mobile */}
-            <li className="md:hidden border-t border-gray-700 pt-4 mt-2">
-              <button
-                onClick={() => {
-                  auth ? logOut() : logIn();
-                  setMenuOpen(false);
-                }}
-                className="w-full bg-white flex items-center text-gray-700 font-bold justify-center gap-x-2 text-sm rounded-lg hover:bg-gray-100 duration-300 transition-colors border px-4 py-2.5 cursor-pointer"
-              >
-                {auth ? "Log Out" : "Log In"}
-              </button>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+              {/* Dropdown Mobile */}
+              <li>
+                <button
+                  onClick={() => setDropdownOpen(!dropdownOpen)}
+                  className="flex text-white items-center w-full py-2 px-3 rounded hover:bg-gray-700 cursor-pointer transition-colors"
+                >
+                  Temáticas{" "}
+                  <FaAngleDown
+                    className={`ml-1 transition-transform ${
+                      dropdownOpen ? "rotate-180" : ""
+                    }`}
+                  />
+                </button>
+
+                {dropdownOpen && (
+                  <div className="mt-2 ml-4">
+                    <NavLink
+                      to="/gymexercise"
+                      className="block px-4 py-2 text-gray-300 hover:text-white hover:bg-gray-700 rounded-lg transition-colors"
+                      onClick={() => {
+                        setDropdownOpen(false);
+                        setMenuOpen(false);
+                      }}
+                    >
+                      Entrenamiento Personal
+                    </NavLink>
+                  </div>
+                )}
+              </li>
+
+              <li>
+                <NavLink
+                  to="/nosotros"
+                  className="block text-white py-2 px-3 rounded hover:bg-gray-700 transition-colors"
+                  onClick={() => setMenuOpen(false)}
+                >
+                  Nosotros
+                </NavLink>
+              </li>
+
+              <li>
+                <NavLink
+                  to="/contacto"
+                  className="block text-white py-2 px-3 rounded hover:bg-gray-700 transition-colors"
+                  onClick={() => setMenuOpen(false)}
+                >
+                  Contacto
+                </NavLink>
+              </li>
+
+              {auth && (
+                <li>
+                  <NavLink
+                    to="/admin"
+                    className="block text-white py-2 px-3 rounded hover:bg-gray-700 transition-colors"
+                    onClick={() => setMenuOpen(false)}
+                  >
+                    Admin
+                  </NavLink>
+                </li>
+              )}
+
+              {/* Boton login mobile */}
+              <li className="border-t-2 border-gray-700 pt-4 mt-2">
+                <button
+                  onClick={() => {
+                    auth ? logOut() : logIn();
+                    setMenuOpen(false);
+                  }}
+                  className="w-full bg-white text-gray-700 font-bold text-sm rounded-lg hover:bg-gray-100 duration-300 transition-colors border px-4 py-2.5 cursor-pointer"
+                >
+                  {auth ? "Log Out" : "Log In"}
+                </button>
+              </li>
+            </ul>
+          </div>
+        )}
+      </nav>
+    </>
   );
 }
--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -1,13 +1,78 @@
-
-import { NavLink } from 'react-router-dom';
-import entrenamiento from '../assets/entrenamiento.jpg';
-import '../styles/home.css'
+import { NavLink } from "react-router-dom";
+import entrenamiento from "../assets/entrenamiento.jpg";
+import dumbbell from "../assets/dumbbell.webp";
+import "../styles/home.css";
 
 export const HomePage = () => {
   return (
     <>
-    <div className='mainContainer'>
-      <div className="bg-onix block max-w-md p-8 md:p-10 rounded-lg shadow-xl transition-all duration-300 hover:shadow-2xl">
+      <main>
+        <section class="relative w-full h-[400px] md:h-[500px] lg:h-[600px] bg-[#0a0a0a] overflow-hidden flex items-center justify-center">
+          <div class="absolute top-[-10%] left-[-10%] w-[500px] h-[500px] rounded-full bg-red-900/20 blur-[120px] z-0"></div>
+          <div class="absolute bottom-[-10%] right-[-5%] w-[600px] h-[600px] rounded-full bg-red-600/10 blur-[150px] z-0"></div>
+
+          <div class="absolute inset-0 w-full h-full z-0">
+            <img
+              src={dumbbell}
+              alt="dumbbell"
+              className="w-full h-full  object-contain mx-auto opacity-65"
+            />
+          </div>
+
+          <div class="relative z-10 text-center px-4">
+            <h1 class="text-6xl md:text-8xl font-black text-white tracking-tighter uppercase italic">
+              Rewrite{" "}
+              <span class="text-transparent bg-clip-text bg-gradient-to-r from-red-500 to-red-800">
+                your limits
+              </span>
+            </h1>
+            <p class="mt-4 text-gray-400 text-lg max-w-md mx-auto">
+              Entrenamiento con IA para elevar tu máximo potencial.
+            </p>
+            <NavLink
+              to="/gymexercise"
+              className="inline-flex w-1/4 justify-center items-center text-gray-200 bg-red-700 hover:bg-red-800 shadow-lg hover:shadow-xl font-bold leading-5 rounded-lg text-base px-6 py-3.5 focus:outline-none transition-all duration-200 transform "
+            >
+              Entrene IA!
+            </NavLink>
+          </div>
+        </section>
+
+        {/* <section className="relative w-full h-[400px] md:h-[500px] lg:h-[600px]  mb-12">
+        
+
+          
+          <div className="absolute inset-0 w-full h-full">
+            <img
+              src={dumbbell}
+              alt="dumbbell"
+              className=" object-cover"
+              priority
+            />
+          </div>
+
+       
+          <div className="relative z-10 h-full flex flex-col items-center justify-center text-center px-4 md:px-8">
+            <h1 className="text-4xl md:text-6xl lg:text-8xl xl:text-9xl text-winered mb-4 md:mb-6">
+              Personal Trainer
+            </h1>
+            <h2 className="text-sm md:text-base  lg:text-lg text-white mb-6 md:mb-8 max-w-2xl">
+              Recordá que debés seguir las instrucciones de la mejor manera para{" "}
+              <span className="text-white font-semibold">
+                resultados óptimos
+              </span>
+              .
+            </h2>
+            <NavLink
+              to="/gymexercise"
+              className="inline-flex w-1/4 justify-center items-center text-gray-200 bg-red-700 hover:bg-red-800 shadow-lg hover:shadow-xl font-bold leading-5 rounded-lg text-base px-6 py-3.5 focus:outline-none transition-all duration-200 transform "
+            >
+              Entrene IA!
+            </NavLink>
+          </div>
+        </section> */}
+
+        {/* <div className="bg-onix block max-w-md p-8 md:p-10 rounded-lg shadow-xl transition-all duration-300 hover:shadow-2xl">
         <img className="rounded-lg w-full h-auto shadow-lg mb-6" src={entrenamiento} alt="Entrenamiento" />
         
         <h5 className="mb-4 text-3xl md:text-4xl font-bold tracking-tight text-white text-center">
@@ -29,10 +94,8 @@ export const HomePage = () => {
         <NavLink to='/gymexercise' className="inline-flex w-full justify-center items-center text-gray-200 bg-red-700 hover:bg-red-800 shadow-lg hover:shadow-xl font-bold leading-5 rounded-lg text-base px-6 py-3.5 focus:outline-none transition-all duration-200 transform ">
           Entrene IA!
         </NavLink>
-      </div>
-    </div>
-    
-
+      </div> */}
+      </main>
     </>
-  )
-}
+  );
+};
